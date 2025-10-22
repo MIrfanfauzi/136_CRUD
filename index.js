e7caac9c978eadaf24f2155b55e452ee83730b81
Irfan Fauzi <irfanfauziyk@gmail.com>
2025-10-22 11:23:50 +0700

    membuat koneksi ke database

--- index.js
+++ index.js
@@ -13,4 +13,19 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
+const db = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    port: '3309',
+    password: 'irfanfauzi',
+    database: 'mahasiswa'
+});
 
+db.connect((err) => {
+  if (err) throw err;{
+    console.error('error Connected to database!' + err.stack);
+    return;
+    }
+    console.log('Connected Successfully!');
+
+});
