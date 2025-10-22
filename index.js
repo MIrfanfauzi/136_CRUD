9084dfde9d865f9f9ccd9f5967382a8d718015d7
Irfan Fauzi <irfanfauziyk@gmail.com>
2025-10-22 11:28:49 +0700

    menambahkan endpoint put untuk mahasiswa

--- index.js
+++ index.js
@@ -55,3 +55,20 @@ app.post('/api/mahasiswa', (req, res) => {
         res.status(201).json({ message: 'User Created Successfuly ', id: results.insertId });
     });
 });
+
+app.put('/api/mahasiswa/:id', (req, res) => {
+    const userId  = req.params.id;
+    const { nama, alamat, agama } = req.body;
+    db.query('UPDATE biodata SET nama = ?, alamat = ?, agama = ? WHERE id = ?',
+        [nama, alamat, agama, userId],
+        (err, results) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ error: 'Database error' });   
+            }
+            res.json({ message: 'User updated successfully' });
+        }
+    );
+});
+
+
